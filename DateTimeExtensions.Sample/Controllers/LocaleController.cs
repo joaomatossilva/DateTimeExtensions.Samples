cfb4c8f446ff0e8a94b05ba67edfc3be2bdd61fb
Joao Silva <kappy@acydburne.com.pt>
2012-03-24 18:19:22 +0000

    Added Locale controler with basic index action and surface layout

---
+++ DateTimeExtensions.Sample/Controllers/LocaleController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DateTimeExtensions.Sample.Controllers
+{
+    public class LocaleController : Controller
+    {
+        //
+        // GET: /Locale/
+
+        public ActionResult Index() {
+        	ViewBag.Locale = System.Threading.Thread.CurrentThread.CurrentCulture.Name;
+            return PartialView();
+        }
+
+    }
+}
