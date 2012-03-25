e3981fa64600e1bcefedee6121d9c3f8bd6e10bd
Joao Silva <kappy@acydburne.com.pt>
2012-03-25 23:14:20 +0100

    added change Locale feature

--- DateTimeExtensions.Sample/Controllers/LocaleController.cs
+++ DateTimeExtensions.Sample/Controllers/LocaleController.cs
@@ -1,20 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using DateTimeExtensions.Sample.Models;
+
 namespace DateTimeExtensions.Sample.Controllers
 {
-    public class LocaleController : Controller
-    {
-        //
-        // GET: /Locale/
+	public class LocaleController : Controller
+	{
+		//
+		// GET: /Locale/
 
-        public ActionResult Index() {
-        	ViewBag.Locale = System.Threading.Thread.CurrentThread.CurrentCulture.Name;
-            return PartialView();
-        }
+		public ActionResult Index() {
+			var localeName = System.Threading.Thread.CurrentThread.CurrentCulture.Name;
+			ViewBag.Locale = localeName;
+			return PartialView();
+		}
 
-    }
+		public ActionResult SelectLocale() {
+			var locales = new List<SelectLocaleViewModel> {
+				new SelectLocaleViewModel { LocaleName = "en-US", HasHolidays = true, HasNaturalText = true },
+				new SelectLocaleViewModel { LocaleName = "en-GB", HasHolidays = true, HasNaturalText = true },
+				new SelectLocaleViewModel { LocaleName = "pt-PT", HasHolidays = true, HasNaturalText = true },
+				new SelectLocaleViewModel { LocaleName = "pt-BR", HasHolidays = true, HasNaturalText = true },
+				new SelectLocaleViewModel { LocaleName = "fr-FR", HasHolidays = true, HasNaturalText = true },
+				new SelectLocaleViewModel { LocaleName = "de-DE", HasHolidays = true, HasNaturalText = true },
+				new SelectLocaleViewModel { LocaleName = "es-ES", HasHolidays = true, HasNaturalText = true },
+				new SelectLocaleViewModel { LocaleName = "da-DK", HasHolidays = true },
+				new SelectLocaleViewModel { LocaleName = "fi-FI", HasHolidays = true },
+				new SelectLocaleViewModel { LocaleName = "is-IS", HasHolidays = true },
+				new SelectLocaleViewModel { LocaleName = "nb-NO", HasHolidays = true },
+				new SelectLocaleViewModel { LocaleName = "nl-NL", HasHolidays = true },
+				new SelectLocaleViewModel { LocaleName = "sv-SE", HasHolidays = true }
+			};
+			return PartialView(locales);
+		}
+
+		public ActionResult SetLocale(string localeName) {
+			Session["localeName"] = localeName;
+			return this.Redirect("/");
+		}
+	}
 }
---
+++ DateTimeExtensions.Sample/Models/SelectLocaleViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DateTimeExtensions.Sample.Models {
+	public class SelectLocaleViewModel {
+		public string LocaleName { get; set; }
+
+		public bool HasHolidays { get; set; }
+
+		public bool HasNaturalText { get; set; }
+	}
+}
