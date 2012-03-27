584578e2e46886c16b35b7967b1bc5ad5d8c4273
Joao Matos Silva <kappy@acydburne.com.pt>
2012-03-27 14:05:15 +0100

    format file

--- DateTimeExtensions.Sample/Controllers/CalendarController.cs
+++ DateTimeExtensions.Sample/Controllers/CalendarController.cs
@@ -6,64 +6,55 @@ using System.Web.Mvc;
 using DateTimeExtensions.Sample.Models.Calendar;
 using System.Globalization;
 
-namespace DateTimeExtensions.Sample.Controllers
-{
-    public class CalendarController : Controller
-    {
-        DateTimeCultureInfo dateTimeCultureInfo = new DateTimeCultureInfo();
-        //
-        // GET: /Calendar/
+namespace DateTimeExtensions.Sample.Controllers {
+	public class CalendarController : Controller {
+		DateTimeCultureInfo dateTimeCultureInfo = new DateTimeCultureInfo();
+		//
+		// GET: /Calendar/
 
-        public ActionResult Index(int year)
-        {
-            var years = new List<int>();
-            for(int i = 0; i < 5; i++){
-                years.Add(DateTime.Now.Year -2 + i);
-            }
-            if (!years.Contains(year))
-            {
-                years.Add(year);
-            }
-            ViewBag.CurrentYear = year;
-            ViewBag.Years = years;
-            return View();
-        }
+		public ActionResult Index(int year) {
+			var years = new List<int>();
+			for (int i = 0; i < 5; i++) {
+				years.Add(DateTime.Now.Year - 2 + i);
+			}
+			if (!years.Contains(year)) {
+				years.Add(year);
+			}
+			ViewBag.CurrentYear = year;
+			ViewBag.Years = years;
+			return View();
+		}
 
-        public ActionResult YearCalendar(int year)
-        {
-            var yearViewModel = new YearViewModel { 
-                YearName = year.ToString(), 
-                Months = BuildMonthsViewModel(year).ToList() };
-            return PartialView(yearViewModel);
-        }
+		public ActionResult YearCalendar(int year) {
+			var yearViewModel = new YearViewModel {
+				YearName = year.ToString(),
+				Months = BuildMonthsViewModel(year)
+			};
+			return PartialView(yearViewModel);
+		}
 
-        private IEnumerable<MonthViewModel> BuildMonthsViewModel(int year){
-            for (int i = 1; i <= 12; i++)
-            {
-                var startMonthDate = new DateTime(year, i, 1);
-                yield return new MonthViewModel
-                {
-                    Name = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(i),
-                    StartDayOffset = (int)startMonthDate.DayOfWeek,
-                    Days = BuildDaysForMonthViewModel(year, i).ToList(),
-                    EndDaysOffset = 6 - (int)startMonthDate.LastDayOfTheMonth().DayOfWeek
-                };
-            }
-        }
+		private IEnumerable<MonthViewModel> BuildMonthsViewModel(int year) {
+			for (int i = 1; i <= 12; i++) {
+				var startMonthDate = new DateTime(year, i, 1);
+				yield return new MonthViewModel {
+					Name = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(i),
+					StartDayOffset = (int)startMonthDate.DayOfWeek,
+					Days = BuildDaysForMonthViewModel(year, i),
+					EndDaysOffset = 6 - (int)startMonthDate.LastDayOfTheMonth().DayOfWeek
+				};
+			}
+		}
 
-        private IEnumerable<DayViewModel> BuildDaysForMonthViewModel(int year, int month)
-        {
-            var daysInMonth = CultureInfo.CurrentCulture.Calendar.GetDaysInMonth(year, month);
-            for (int i = 1; i <= daysInMonth; i++)
-            {
-                var day = new DateTime(year, month, i);
-                yield return new DayViewModel
-                {
-                    DayText = i.ToString(),
-                    IsWorkingDay = dateTimeCultureInfo.IsWorkingDay(day.DayOfWeek),
-                    IsHoliday = dateTimeCultureInfo.GetHolidaysOfYear(year).Where(h => h.IsInstanceOf(day)).SingleOrDefault() != null
-                };
-            }
-        }
-    }
+		private IEnumerable<DayViewModel> BuildDaysForMonthViewModel(int year, int month) {
+			var daysInMonth = CultureInfo.CurrentCulture.Calendar.GetDaysInMonth(year, month);
+			for (int i = 1; i <= daysInMonth; i++) {
+				var day = new DateTime(year, month, i);
+				yield return new DayViewModel {
+					DayText = i.ToString(),
+					IsWorkingDay = dateTimeCultureInfo.IsWorkingDay(day.DayOfWeek),
+					IsHoliday = dateTimeCultureInfo.GetHolidaysOfYear(year).Where(h => h.IsInstanceOf(day)).SingleOrDefault() != null
+				};
+			}
+		}
+	}
 }
