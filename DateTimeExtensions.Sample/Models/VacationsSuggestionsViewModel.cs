080373b2cde05dd55ac2360fe21898ecfb53fb72
Joao Matos Silva <kappy@acydburne.com.pt>
2012-03-28 15:36:09 +0100

    Added Vacation Suggestion features

---
+++ DateTimeExtensions.Sample/Models/VacationsSuggestionsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DateTimeExtensions.Sample.Models {
+	public class VacationsSuggestionsViewModel {
+		public int TotalDaysOff { get; set; }
+		public IEnumerable<VacationSuggestion> Suggestions { get; set; }
+	}
+
+	public class VacationSuggestion {
+		public DateTime StartDate { get; set; }
+		public DateTime EndDate { get; set; }
+		public int VacationDaysSpent { get; set; }
+	}
+}
