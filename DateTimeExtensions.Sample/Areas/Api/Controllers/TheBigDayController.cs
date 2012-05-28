3ef94194299d571977a7677e48504f91253cba86
Joao Silva <kappy@acydburne.com.pt>
2012-05-28 21:50:32 +0100

    ** Personal Stuff **

---
+++ DateTimeExtensions.Sample/Areas/Api/Controllers/TheBigDayController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DateTimeExtensions.Sample.Areas.Api.Controllers {
+	public class TheBigDayController : ApiController {
+		public string Get() {
+			DateTimeCultureInfo ptCultureInfo = new DateTimeCultureInfo("pt-PT");
+			var now = DateTime.Now;
+			return now.ToNaturalText(new DateTime(2012, 6, 16), true, ptCultureInfo);
+		}
+	}
+}
