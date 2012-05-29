c177e260ca04fb7bf407fb7f673e140a69b5f5b7
Joao Matos Silva <kappy@acydburne.com.pt>
2012-05-29 09:28:44 +0100

    ** Personal Stuff **

---
+++ DateTimeExtensions.Sample/Controllers/TheBigDayController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DateTimeExtensions.Sample.Controllers
+{
+    public class TheBigDayController : Controller
+    {
+        //
+        // GET: /TheBigDay/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
