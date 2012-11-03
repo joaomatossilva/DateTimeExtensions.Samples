18b7f49ae3320938da3b2e3098e111559f7e73e3
Joao Matos Silva <kappy@acydburne.com.pt>
2012-11-03 16:39:10 +0000

    Added datetimeextensions lib version on header

---
+++ DateTimeExtensions.Sample/Controllers/VersionController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DateTimeExtensions.WorkingDays;
+
+namespace DateTimeExtensions.Sample.Controllers
+{
+    public class VersionController : Controller
+    {
+        //
+        // GET: /Version/
+
+        public ActionResult Index()
+        {
+            var assembly = typeof (IWorkingDayCultureInfo).Assembly;
+            ViewBag.Version = assembly.GetName().Version;
+            return PartialView();
+        }
+
+    }
+}
